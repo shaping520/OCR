33913f3c48f490943e90657eedce7ea48994a937
shaping520 <144744478+shaping520@users.noreply.github.com>
2026-02-12 19:56:02 +0800

    Set up android_logger in helloFromRust function
    Initialize android_logger and log info message.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
+use android_logger::Config;
+use log::Level;
+
 #[no_mangle]
-pub extern "system" fn Java_top_yogiczy_mytv_RustBridge_helloFromRust(
-) {
-    println!("Rust function called successfully!");
+pub extern "system" fn Java_top_yogiczy_mytv_RustBridge_helloFromRust() {
+    android_logger::init_once(
+        Config::default().with_min_level(Level::Info)
+    );
+
+    log::info!("Hello from Rust Android SO!");
 }
